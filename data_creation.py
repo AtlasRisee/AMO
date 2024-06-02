4615ae5c48c9d417e162129e650aaedf97a05710
atlas <stud0000279804@utmn.ru>
2024-06-02 12:38:25 +0500

    2. Вынести константы

--- data_creation.py
+++ data_creation.py
@@ -3,46 +3,44 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+# Constants
+NUM_HOURS_IN_DAY = 24
+NUM_DAYS_IN_YEAR = 365
+MIN_ENERGY_CONSUMPTION = 100
+MAX_ENERGY_CONSUMPTION = 500
+TEST_SIZE = 0.1
+RANDOM_STATE = 101
+
+def generate_data(start_date, num_hours_in_day, num_days_in_year, min_energy_consumption, max_energy_consumption):
+    num_hours = num_hours_in_day * num_days_in_year
+    energy_consumption = np.random.randint(min_energy_consumption, max_energy_consumption, num_hours)
+    date_range = pd.date_range(start=start_date, periods=num_hours, freq='H')
+    data = {'date': date_range, 'energy_consumption': energy_consumption}
+    return pd.DataFrame(data)
+
+def split_data(df, test_size, random_state):
+    X = df['date']
+    y = df['energy_consumption']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return X_train, X_test, y_train, y_test
+
+def save_data(df, directory, filename):
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    df.to_csv(os.path.join(directory, filename), index=False)
 
 if __name__ == "__main__":
-
     print("1. data_creation.py\n")
 
-    # Generate data on the amount of electricity consumed every hour during the day
-    num_hours = 24
-    days = 365
-    energy_consumption = np.random.randint(100, 500, num_hours * days)
-
-    # Create DataFrame for storing generated data
-    date_range = pd.date_range(start='1/1/2024', periods=num_hours * days, freq='H')
-    data = {'date': date_range, 'energy_consumption': energy_consumption}
-    df = pd.DataFrame(data)
-
-    # Output information about df.DataFrame
+    df = generate_data('1/1/2024', NUM_HOURS_IN_DAY, NUM_DAYS_IN_YEAR, MIN_ENERGY_CONSUMPTION, MAX_ENERGY_CONSUMPTION)
     df.info()
 
-    # Divided into training and test sample
-    X_train, X_test, y_train, y_test = train_test_split(df['date'], df['energy_consumption'], test_size=0.1, random_state=101)
+    X_train, X_test, y_train, y_test = split_data(df, TEST_SIZE, RANDOM_STATE)
     print('len(X_train):', len(X_train))
     print('len(X_test):', len(X_test))
 
-    # Save the training dataset
-    if not os.path.isdir('data'):
-        os.mkdir('data')
+    df_train = pd.DataFrame({'date': X_train, 'energy_consumption': y_train})
+    df_test = pd.DataFrame({'date': X_test, 'energy_consumption': y_test})
 
-    if not os.path.isdir('data/train'):
-        os.mkdir('data/train')
-
-    df_train = pd.DataFrame()
-    df_train['date'] = X_train
-    df_train['energy_consumption'] = y_train
-    df_train.to_csv('data/train/train.csv', index=False)
-
-    # Saving the test dataset
-    if not os.path.isdir('data/test'):
-        os.mkdir('data/test')
-
-    df_test = pd.DataFrame()
-    df_test['date'] = X_test
-    df_test['energy_consumption'] = y_test
-    df_train.to_csv('data/test/test.csv', index=False)
+    save_data(df_train, 'data/train', 'train.csv')
+    save_data(df_test, 'data/test', 'test.csv')
